6c1a5bc2643fd4988028820782c5b5df7684afe1
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2023-10-05 18:40:46 -0300

    insert record

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,6 +1,6 @@
 <h1>Listagem dos suportes</h1>
 
-<a href="{{route('supports.store')}}">Criar Dúvida</a>
+<a href="{{route('supports.create')}}">Criar Dúvida</a>
 
 <table>
     <thead>
@@ -9,12 +9,12 @@
         <th>Descrição</th>
         <th></th>
     </thead>
-    <tbody>
-        @foreach ($supports as $support)
+    @foreach ($supports as $support)
+        <tbody>
             <td>{{ $support->subject }}</td>
             <td>{{ $support->status }}</td>
             <td>{{ $support->body }}</td>
             <td></td>
-        @endforeach
-    </tbody>
+        </tbody>
+    @endforeach
 </table>
